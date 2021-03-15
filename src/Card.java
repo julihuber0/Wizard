ba78410a42f6f59072a6081dfed76c898508fb83
SliceLetsPlay <56352808+SliceLetsPlay@users.noreply.github.com>
2021-03-16 00:41:33 +0100

    New version

---
+++ src/Card.java
@@ -0,0 +1,26 @@
+public class Card {
+
+    private String value;
+    private Color color;
+
+    public Card(String value, Color color) {
+        this.value = value;
+        this.color = color;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
---
+++ src/Color.java
@@ -0,0 +1,3 @@
+public enum Color {
+    GREEN, BLUE, RED, YELLOW
+}
