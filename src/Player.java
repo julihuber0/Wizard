6e36afde9f4bd09821c9a5448fef7fcfb41bfc2d
SliceLetsPlay <56352808+SliceLetsPlay@users.noreply.github.com>
2021-03-16 13:57:54 +0100

    New version

--- src/Player.java
+++ src/Player.java
@@ -4,4 +4,36 @@ public class Player {
     private int points;
     private int saidStitches;
     private int currentStitches;
+
+    public Player(String name)
+    {
+        this.name=name;
+        points = 0;
+        saidStitches=0;
+        currentStitches=0;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getSaidStitches() {
+        return saidStitches;
+    }
+
+    public void setSaidStitches(int saidStitches) {
+        this.saidStitches = saidStitches;
+    }
+
+    public int getCurrentStitches() {
+        return currentStitches;
+    }
+
+    public void setCurrentStitches(int currentStitches) {
+        this.currentStitches = currentStitches;
+    }
 }
