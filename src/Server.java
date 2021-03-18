79b1277621de41e135f6b154c74e45119c00bfed
Tobias <eder.tobias2@gmail.com>
2021-03-18 21:50:30 +0100

    TobiasEd

--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,33 @@
+import java.util.ArrayList;
+
 public class Server {
 
 
     /**
-     * bietet an
+     * Bietet Broadcast (BC) Methoden an
      *
-     *
+     * Methoden, die allgemein für alle Spieler zählen z.B. Trumpffarbe, Scoreboard etc.
      *
      */
+
+    /**
+     * Sendet immer ein zweistelliges Kürzel gefolgt von einem Strichpunkt an die Clients zur Identifikation
+     *
+     * SC: Scoreboard
+     *
+     */
+
+    public void bcScoreboard(ArrayList<Player> players) {
+        //zieht sich den Playername und den zugehörigen Score
+
+        //formatiert ihn zu einem String (wird später in den Clients wieder decoded
+        String score = "SC;";
+        //broadcastet den an alle Spieler
+
+        for (Player p:players) {
+
+
+        }
+    }
+
 }
