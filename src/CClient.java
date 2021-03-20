c86c6d87a9c252121a32d9c2b6a6724eae9aad89
Tobias <eder.tobias2@gmail.com>
2021-03-20 16:49:30 +0100

    TobiasEd

---
+++ src/CClient.java
@@ -0,0 +1,48 @@
+import ea.Client;
+
+public class CClient extends Client {
+
+    /**
+     * Der Name des Clients, z.B. "Herbert".
+     */
+    private String name;
+
+    public CClient(String ipAdress, String name) {
+        //Verbinde dich mit der übergebenen IP-Adresse und Port 7654.
+        //Port 7654 ist willkürlich gewählt. Wichtig ist,
+        //  - dass die Portnummer größer als 1024 ist.
+        //  - dass der Client dieselbe Portnummer hat.
+        super(ipAdress, 7654);
+        this.name = name;
+
+        //Warten, bis die Verbindung zum Server steht:
+        //   Achtung: Wenn kein Server gefunden werden kann,
+        //            friert dieser Konstruktor ein!
+        warteAufVerbindung();
+
+    }
+
+
+
+    @Override
+    public void empfangeString(String string) {
+        //empfangenen String einfach an der Konsole ausgeben
+        System.out.println("[Client hat empfangen:] " + string);
+
+        String key = string.substring(0,2);
+        String content = string.substring(3);
+        switch (key) {
+            case "NF":
+                //Namen zurücksenden
+
+                break;
+
+            default:
+                break;
+
+        }
+
+    }
+
+
+}
