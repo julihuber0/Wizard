41b01327b07b8906f40483dcff56ec7af91ba99b
Tobias <eder.tobias2@gmail.com>
2021-03-20 23:22:21 +0100

    TobiasEd

--- src/ColorW.java
+++ src/ColorW.java
@@ -1,3 +1,18 @@
 public enum ColorW {
     GREEN, BLUE, RED, YELLOW;
+
+    public ColorW toEnum(String s) {
+        if(s.equals("GREEN")) {
+            return ColorW.GREEN;
+        }
+        if(s.equals("BLUE")) {
+            return ColorW.BLUE;
+        }
+        if(s.equals("RED")) {
+            return ColorW.RED;
+        }
+        else {
+            return ColorW.YELLOW;
+        }
+    }
 }
