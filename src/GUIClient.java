026f2a6f5c865224e830de2b2cf797241ffbbcd4
Tobias <eder.tobias2@gmail.com>
2021-03-27 23:23:12 +0100

    TobiasEd

--- src/GUIClient.java
+++ src/GUIClient.java
@@ -295,6 +295,10 @@ public class GUIClient extends Game implements MausReagierbar {
 
     public void updateSaidStitches()
     {
+        for(Player p:players){
+            System.out.println(p.getName() + "  " + p.getId());
+        }
+
         for(Player p:players)
             if(p.getId()==idSelf)
             {
