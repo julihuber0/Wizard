4a81d500d920a2a58d2b10bd7385c7f68574ac93
SliceLetsPlay <56352808+SliceLetsPlay@users.noreply.github.com>
2021-04-03 14:41:04 +0200

    New version

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import ea.Text;
 
 public class Main {
     public static void main(String[] args) {
-        //GUIClient gui = new GUIClient();
-        Text.geladeneSchriftartenAusgeben();
+        GUIClient gui = new GUIClient();
     }
 }
