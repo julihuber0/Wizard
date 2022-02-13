e2c0b062391da858789bf5abe53ec62e5ef657aa
Tobias <eder.tobias2@gmail.com>
2022-02-13 01:23:13 +0100

    a

--- src/view/Utility.java
+++ src/view/Utility.java
@@ -1,13 +1,11 @@
 package view;
 
-import model.ColorW;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class Utility {
     public static float SAMPLE_RATE = 8000f;
@@ -30,39 +28,24 @@ public class Utility {
         JOptionPane.showMessageDialog(null, msg);
     }
 
-    public static void tone(int hz, int msecs, double vol)
-            throws LineUnavailableException
-    {
-        byte[] buf = new byte[1];
-        AudioFormat af =
-                new AudioFormat(
-                        SAMPLE_RATE, // sampleRate
-                        8,           // sampleSizeInBits
-                        1,           // channels
-                        true,        // signed
-                        false);      // bigEndian
-        SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
-        sdl.open(af);
-        sdl.start();
-        for (int i=0; i < msecs*8; i++) {
-            double angle = i / (SAMPLE_RATE / hz) * 2.0 * Math.PI;
-            buf[0] = (byte)(Math.sin(angle) * 127.0 * vol);
-            sdl.write(buf,0,1);
-        }
-        sdl.drain();
-        sdl.stop();
-        sdl.close();
+    public static void playPingSound() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        File f = new File("./Resources/notification.wav");
+        AudioInputStream audioIn = AudioSystem.getAudioInputStream(f);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioIn);
+        clip.start();
     }
-    public static void main(String[] args) throws Exception {
-        Utility.tone(1000,100,0.3);
-        Thread.sleep(1000);
-        Utility.tone(100,1000,0.3);
-        Thread.sleep(1000);
-        Utility.tone(5000,100,0.3);
-        Thread.sleep(1000);
-        Utility.tone(400,500,0.3);
-        Thread.sleep(1000);
-        Utility.tone(400,500, 0.2);
+
+    public static void main(String[] args){
+        try {
+            playPingSound();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
 
     }
 }
