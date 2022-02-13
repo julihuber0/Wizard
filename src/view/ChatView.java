aa32f08bba151c7b5f80ac04d228565389b82a4f
julihuber0 <julianhuber162@gmail.com>
2022-02-13 02:37:05 +0100

    Made it work

--- src/view/ChatView.java
+++ src/view/ChatView.java
@@ -15,9 +15,7 @@ public class ChatView extends JPanel {
             msg[i] = new JLabel(" ");
             msg[i].setFont(msg[i].getFont().deriveFont(Font.PLAIN));
             add(msg[i]);
-            msg[i].setAlignmentX(0.0f);
         }
-        this.setAlignmentX(0.0f);
         sender = new ChatSender(gui);
         add(sender);
     }
@@ -25,8 +23,6 @@ public class ChatView extends JPanel {
     public void updateChat(ArrayList<String> chat) {
         for(int i = 9; i>=10-chat.size(); i--) {
             msg[i].setText(chat.get(i-10+chat.size()));
-            msg[i].setAlignmentX(0.0f);
         }
-        this.setAlignmentX(0.0f);
     }
 }
