599ed9a19ea14c12295834f8406da230c7842512
Tobias <eder.tobias2@gmail.com>
2022-02-22 18:41:00 +0100

    a

--- src/model/GameW.java
+++ src/model/GameW.java
@@ -251,6 +251,7 @@ public class GameW implements Runnable {
     public void start() {
         setPlayerCount(players.size());
         setMaxRounds(players.size());
+        currentRound = 9;
         startNextRound();
     }
 
